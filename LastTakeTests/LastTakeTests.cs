a1fa106a9f97b918a5e350fcd2216277a1fa2a53
lily <s395135@sinyi.com.tw>
2023-05-10 15:05:49 +0800

    Reafactor:將測試建立的集合sequece都抽出去。

--- LastTakeTests/LastTakeTests.cs
+++ LastTakeTests/LastTakeTests.cs
@@ -6,13 +6,14 @@ namespace LastTakeTests
     [TestClass]
     public class LastTaskTests
     {
+        //將TestMethod裡面的List都抽出來，寫成欄位
+        private readonly List<int> _sequence = new List<int>() { 1, 2, 3, 4, 5, 6 };
+        
         //情境一、取出集合最後三個值
         [TestMethod]
         public void GivenSequenceTakeLastThree()
         {
-            var sequence = new List<int>() { 1, 2, 3, 4, 5, 6};
-
-            var result = sequence.LastTakeNumber(3);
+            var result = _sequence.LastTakeNumber(3);
 
             CollectionAssert.AreEqual(new List<int>{ 4, 5, 6 }, result);//卡在這裡，因為結果是集合，要用CollectionAssert而非Assert
         }
@@ -21,9 +22,7 @@ namespace LastTakeTests
         [TestMethod]
         public void GivenSequenceTakeLastTwo()
         {
-            var sequence = new List<int>() { 1, 2, 3, 4, 5, 6 };
-
-            var result = sequence.LastTakeNumber(2);
+            var result = _sequence.LastTakeNumber(2);
 
             CollectionAssert.AreEqual(new List<int> { 5, 6 }, result);
         }
